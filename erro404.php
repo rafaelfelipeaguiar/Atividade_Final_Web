ba9bb3cac9ed190adfc8584b423efe863ada89ca
rafaelFelipe <rafaguiar2005@gmail.com>
2022-11-24 09:44:05 -0400

    tomi

--- erro404.php
+++ erro404.php
@@ -15,7 +15,7 @@
 			<div>
                 <div style="background-image: url('images/kkkkk.png'); height: 630px; width: 670px; ">
                 </div>
-				<h2>Oops! Page Not Be Found</h2>
+				<h2>Oops!! Page Not Be Found</h2>
                 <h3>Error 404</h3>
 			</div>
 		</div>
